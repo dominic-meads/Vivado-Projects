67bc735708894af1064956474776d014eb6393c0
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-10-10 11:12:47 -0700

    added tone EN and increased LED high time

--- Button_Sequence_Detect/src/button_detect.v
+++ Button_Sequence_Detect/src/button_detect.v
@@ -4,7 +4,8 @@
 //
 //   PROJECT DESCRIPTION:	A basic sequence detector of which buttons are pressed. If the 
 //							correct sequence is detected and the unlock switch goes high,
-// 							a blue LED will light, if not, the red LED will light. There
+// 							a blue LED will light, and a 362 Hz tone will sound. If not, 
+//					        the red LED will light, and a 110 Hz tone will sound. There
 //                          is no overlap. The buttons I am using are 4 buttons built into
 //                          the Arty z7-10 board.
 //
@@ -17,12 +18,14 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 
 module button_detect(
-	input clk,         // 125 MHz
-	input [3:1] BTN,   // 4 buttons on Arty Z7-10: BTN3, BTN2, BTN1, BTN0. The first 3 are the sequence inputs
-	input clr_n,       // ACTIVE LOW clear/reset
-	input enter,       // press to see if sequence is correct(BTN0)
-	output blue,       // sequence entry correct
-	output red         // sequnce entry incorrect
+	input clk,              // 125 MHz
+	input [3:1] BTN,        // 4 buttons on Arty Z7-10: BTN3, BTN2, BTN1, BTN0. The first 3 are the sequence inputs
+	input clr_n,            // ACTIVE LOW clear/reset
+	input enter,            // press to see if sequence is correct(BTN0)
+	output blue,            // sequence entry correct
+	output red,             // sequnce entry incorrect
+	output reg tone_EN362,  // enables 362 Hz tone
+	output reg tone_EN110   // enables 110 Hz tone
 	);
 	
 	// states
@@ -36,9 +39,9 @@ module button_detect(
 	
 	// registers
 	reg [2:0] STATE = 0;       // current state
-	reg [25:0] rLED_time = 0;  // keeps the red LED lit for 500 ms
+	reg [26:0] rLED_time = 0;  // keeps the red LED lit for 500 ms
 	reg rLED_time_EN = 0;      // enable for red LED_time
-	reg [25:0] bLED_time = 0;  // keeps the blue LED lit for 500 ms
+	reg [26:0] bLED_time = 0;  // keeps the blue LED lit for 500 ms
 	reg bLED_time_EN = 0;      // enable for blue LED_time
 	
 	// Next-state logic of Moore machine with part of output logic integrated in ////////////////////////////////////////////////////
@@ -132,7 +135,7 @@ module button_detect(
 						  end  // got2313
 								
 				INDICATEb : begin
-								if (bLED_time == 62500000)   // stay in "INDICATE" state for 500 ms, THEN return to "RESET"
+								if (bLED_time == 125000000)   // stay in "INDICATE" state for 500 ms, THEN return to "RESET"
 									begin 
 										rLED_time_EN <= 0;  // incorrect sequence light OFF
 										bLED_time_EN <= 0;  // correct sequence indicator OFF 
@@ -147,7 +150,7 @@ module button_detect(
 						    end  // INDICATEb
 							
 				INDICATEr : begin
-								if (rLED_time == 62500000)   // stay in "INDICATE" state for 500 ms, THEN return to "RESET"
+								if (rLED_time == 125000000)   // stay in "INDICATE" state for 500 ms, THEN return to "RESET"
 									begin 
 										rLED_time_EN <= 0;  // incorrect sequence light OFF
 										bLED_time_EN <= 0;  // correct sequence indicator OFF 
@@ -177,13 +180,22 @@ module button_detect(
 		begin 
 			if (rLED_time_EN)
 				begin 
-					if (rLED_time <= 62499999)  // led will be high for 62.5 million counts (low for 1 count @ rLED_time <=0) @ 125 MHz is 500 ms of total counting time
-						rLED_time <= rLED_time + 1;
+					if (rLED_time <= 124999999)  // led will be high for 125 million counts (low for 1 count @ rLED_time <=0) @ 125 MHz is 1 sec of total counting time
+						begin 
+							rLED_time <= rLED_time + 1;
+							tone_EN110 <= 1;  // enable the 110 Hz tone generation for 1 second
+						end  // if (rLED_time...
 					else 
-						rLED_time <= 0;
+						begin 
+							rLED_time <= 0;
+							tone_EN110 <= 0;  // disable the 110 Hz tone generation
+						end  // else 
 				end  // if (rLED_time_EN)
-			else 
-				rLED_time <= 0;
+			else
+				begin 
+					rLED_time <= 0;
+					tone_EN110 <= 0;
+				end  // else 
 		end  // always 
 	// end red
 	// blue 
@@ -191,19 +203,30 @@ module button_detect(
 		begin 
 			if (bLED_time_EN)
 				begin 
-					if (bLED_time <= 62499999)  // led will be high for 62.5 million counts (low for 1 count @ bLED_time <=0) @ 125 MHz is 500 ms of total counting time
-						bLED_time <= bLED_time + 1;
-					else 
-						bLED_time <= 0;
+					if (bLED_time <= 124999999)  // led will be high for 125 million counts (low for 1 count @ rLED_time <=0) @ 125 MHz is 1 sec of total counting time
+						begin 
+							bLED_time <= bLED_time + 1;
+							tone_EN362 <= 1;  // enable the 362 Hz tone generation for 1 second
+						end  // if (bLED_time...
+					else
+						begin 
+							bLED_time <= 0;
+							tone_EN362 <= 0;  // enable the 362 Hz tone generation
+						end  // else 
 				end  // if (bLED_time_EN)
 			else 
-				bLED_time <= 0;
+				begin 
+					bLED_time <= 0;
+					tone_EN362 <= 0;
+				end  // else
 		end  // always
 	// end blue 
 	
-	// output assignments (if the LED enables are low, the LED will be low, otherwise they will be lit for 625 million counts @125 MHz (0.5 seconds)
+	// output assignments (if the LED enables are low, the LED will be low, otherwise they will be lit for 125 million counts @125 MHz (0.5 seconds)
 	assign red = (rLED_time > 0) ? 1:0;
 	assign blue = (bLED_time > 0) ? 1:0;
 	// end 2nd part of output logic (relies on the enables of the LEDs) //////////////////////////////////////////////////////////////
 	
 endmodule  // button_detect
+
+					
